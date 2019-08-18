97c96e222465674fb8af8ae1b7980a89a8d160da
apricotcomic <apricotcomic@gmail.com>
2019-08-18 20:32:31 +0900

    ユーザー登録画面

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,6 +4,12 @@
 </head>
 <body>
     こんにちは！
+    @if (Auth::check())
+        {{ \Auth::user()->name }}さん
+    @else
+        ゲストさん<br />
+        <a href="/auth/register">会員登録</a>
+    @endif
 </body>
 </html>
 
